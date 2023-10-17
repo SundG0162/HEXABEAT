8eda1bca8dc0e470a0135c833a7368532b159749
SundG0162 <akail00162@gmail.com>
2023-10-17 18:21:35 +0900

    Revert "메모장업뎃 : 한글 주석이 github에서 안깨지는지 실험"
    This reverts commit 059ed3f34865d297cffbe9f0bcf648c4e53f9eec.

--- Assets/01.Scripts/Sheet.cs
+++ Assets/01.Scripts/Sheet.cs
@@ -12,12 +12,18 @@ public enum NoteType
     c // 짧은 노트로 이루어져 있는 연속된 노트들 (롱노트와 비슷함)
 }
 
+//arriveTime 구하는법
+//거리 = 시간 x 속도, 즉 시간 = 거리 / 속도
+//노트가 생성되고 판정선까지 가는 거리는 모두 동일
+//속도도 게임 뒤집을거 아니면 모두 동일
+//arriveTime = distance / speed;
+//근데 이거 생성해주는 타이밍은 어케 구함? (진짜모름)
 public struct Note
 {
     public int lineIndex; // 노트가 생성되는 인덱스
     public int arriveTime; // 노트가 판정선에 도달하는 시간 (ms)
     public NoteType noteType; // 노트의 타입
-    public int tail; // 롱노트가 끝나는 시간 (ms)
+    public int tail; // 롱노트의 지속시간
 
     public Note(int lineIndex, int arrivedTime, NoteType noteType, int tail)
     {
